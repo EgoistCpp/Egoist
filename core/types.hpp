c985fa0bf38889d81ad9798c1ae5ab4bc37c7115
UndefinedBehaviourAndIllFormed <egoistcpp@gmail.com>
2022-06-13 05:52:55 +0200

    added types

---
+++ core/types.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#if egoist_major_version >= 0 && egoist_middle_version >= 0 && egoist_minor_version >= 1
+
+namespace egoist
+{
+    using u8 = unsigned char;
+
+    using s8 = signed char;
+
+    using u16 = unsigned short;
+
+    using s16 = signed short;
+    
+    using u32 = unsigned int;
+
+    using s32 = signed int;
+
+    using f32 = float;
+    
+    using u64 = unsigned long int;
+
+    using s64 = signed long int;
+
+    using f64 = double;
+}
+
+#endif
